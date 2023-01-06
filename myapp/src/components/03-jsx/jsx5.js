29b18bc55b667ec20cb773703dd8adb0a3132b17
Emrullah Celebi <cumali20182019@gmail.com>
2023-01-06 09:50:03 +0100

    v1

--- myapp/src/components/03-jsx/jsx5.js
+++ myapp/src/components/03-jsx/jsx5.js
@@ -3,6 +3,7 @@ import React from 'react'
 const Jsx5 = () => {
 
     const names=["Ali","Veli","Deli","Seli"];
+    const cities= ["Istanbul","Izmir","London","Paris"];
 
   return (
     <div>
@@ -11,7 +12,11 @@ const Jsx5 = () => {
             
         </ul>
 
-
+        <select name="" id="">
+            {cities.map((city,i)=><option key={i}>{city}</option>)}
+            
+            <option value="">Istanbul</option>
+        </select>
 
 
     </div>
