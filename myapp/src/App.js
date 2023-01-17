3d4292f370dcd52bfd36a7d710ed6d33afd96bb4
Emrullah Celebi <cumali20182019@gmail.com>
2023-01-17 22:00:55 +0100

    v9

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,4 +1,9 @@
+import { Col, Container, Row } from "react-bootstrap";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/00-home/header/header";
+import Menu from "./components/00-home/menu/menu";
 import HelloWorld from "./components/01-hello-world/hello-world";
+import HelloReact from "./components/02-hello-react/hello-react";
 import Jsx1 from "./components/03-jsx/jsx1";
 import Jsx2 from "./components/03-jsx/jsx2";
 import Jsx3 from "./components/03-jsx/jsx3";
@@ -15,7 +20,6 @@ import Greetings from "./components/06-props-1/greetings";
 import Products from "./components/07-props-2/products";
 import Clock2 from "./components/08-clock2/clock2";
 import Gallery from "./components/09-image/gallery";
-
 import Image from "./components/09-image/image";
 import ProfileCard from "./components/10-profile-card/profile-card";
 import BootstrapDynamic from "./components/11-bootstrap/bootstrap-dynamic";
@@ -26,14 +30,13 @@ import Shop from "./components/14-products/shop";
 import State from "./components/15-usestate/state";
 import Stateless from "./components/15-usestate/stateless";
 import Counter from "./components/16-counter/counter";
-
 import Birthday from "./components/17-birthday/birthday";
 import UseEffect1 from "./components/18-useeffect/useeffect1";
 import UseEffect2 from "./components/18-useeffect/useeffect2";
 import Clock3 from "./components/19-clock3/clock3";
 import CountryFilter from "./components/20-country-filter/country-filter";
 import Scroll from "./components/21-useref/scroll";
-import Useref from "./components/21-useref/useref";
+import UseRef from "./components/21-useref/useref";
 import ClassComp from "./components/22-class-type-component/class-comp";
 import UserCards from "./components/23-fetch-users/user-cards";
 import Countries from "./components/24-axios-countries/countries";
@@ -42,21 +45,35 @@ import Form1 from "./components/26-forms/form1";
 import Form2 from "./components/26-forms/form2";
 import Form3 from "./components/26-forms/form3";
 import Form4 from "./components/26-forms/form4";
-import Odev3 from "./components/replit-odevler/odev-3/replit-odev3";
-
-
+import Form5 from "./components/26-forms/form5";
 
 const App = () => {
   return (
-    <div>
+    <BrowserRouter>
+      <Header />
+      <Container fluid>
+        <Row>
+          <Col sm={2}>
+            <Menu />
+          </Col>
+          <Col sm={10}>
+            <Routes>
+              <Route path="/hello-world" element={<HelloWorld />} />
+              <Route path="/hello-react" element={<HelloReact />} />
+              <Route path="/jsx1" element={<Jsx1 />} />
+              <Route path="/jsx2" element={<Jsx2 />} />
+              <Route path="/jsx3" element={<Jsx3 />} />
+              <Route path="/jsx4" element={<Jsx4 />} />
+              <Route path="/jsx5" element={<Jsx5 />} />
+            </Routes>
+          </Col>
+        </Row>
+      </Container>
       {/*         
-        <HelloWorld/>
-        <Jsx1/>
-        <Jsx2/>
-        <Jsx3/>
-        <Jsx4/>
-        <Jsx5/>
+        
+     
         <Jsx6 />
+        <Jsx7/>
         <Style1/>
         <Style2/>
         <Style3/>
@@ -65,45 +82,36 @@ const App = () => {
         <Greetings/>
         <Products/>
         <Clock2 textColor="#56ff33" bgColor="#0012C3"/>
+        <Image/>
         <Gallery/>
-         <Image/>  
-         <ProfileCard avatar="profile.jpg" name="Ali Gel" location="Turkey, Istanbul" shot="3" followers="23432" following="234"/>   
-<BootstrapStatic/>
-<BootstrapDynamic/>
-<Icon />
- <Events/>
-  <Shop/>
-  <Jsx7/>
-  <Stateless/>
-  <State/>
-  <Counter/>
-
-  <Odev3/>
-   <Birthday/>
-   <UseEffect1/>
-   <Clock3/>
-   <UseEffect2/>
-   <CountryFilter/>
-   <Useref/>
-    <Scroll/>
-    <ClassComp/>
-    <UserCards/>
-    <Countries/>
-    <ParentComp/>
-    <Form1/>
-    <Form2/>
-    <Form3/>
+        <ProfileCard avatar="profile.jpg" name="Ali Gel" location="Turkey, Istanbul" shot="3" followers="23432" following="234"/>
+        <BootstrapStatic/>
+        <BootstrapDynamic/>
+        <Icon/>
+        <Events/>
+        <Shop/>
+        <Stateless/>
+        <State/>
+        <Counter/>
+        <Birthday/>
+        <UseEffect1/>
+        <Clock3/>
+        <UseEffect2/>
+        <CountryFilter/>
+        <UseRef/>
+        <Scroll/>
+        <ClassComp/>
+        <UserCards/>
+        <Countries/>
+        <ParentComp/>
+        <Form1/>
+        <Form2/>
+        <Form3/>
+        <Form4/>
+        <Form5/>
+        <Form6/>
  */}
-
-<Form4/>
-
-
-
-
-     
-    </div>
+    </BrowserRouter>
   );
 };
 export default App;
-
-
