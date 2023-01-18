ea5c5cb52f9f45890fb199d10e3cf0b1b526a9d2
Emrullah Celebi <cumali20182019@gmail.com>
2023-01-18 22:01:51 +0100

    celebi

---
+++ myapp/src/store/index.js
@@ -0,0 +1,8 @@
+import { createContext } from "react";
+
+//Bos bir merkezi state olusturuldu
+
+const StoreContext = createContext();
+
+//Merkezi state disariya acildi
+export default StoreContext;
